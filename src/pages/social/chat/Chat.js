79521270a529140a61e7342d47547a7b45a4854a
Armaankhaan01 <armankhan876426@gmail.com>
2025-03-24 19:15:27 +0530

    Add: ChatList SearchList Chat Sidebar Updated

--- src/pages/social/chat/Chat.js
+++ src/pages/social/chat/Chat.js
@@ -1,5 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './Chat.scss';
+import { getConversationList } from '@redux/api/chat';
+import useEffectOnce from '@hooks/useEffectOnce';
+import ChatList from '@components/chat/list/ChatList';
+
 const Chat = () => {
-  return <div>Chat</div>;
+  const { selectedChatUser, chatList } = useSelector((state) => state.chat);
+  const dispatch = useDispatch();
+
+  useEffectOnce(() => {
+    dispatch(getConversationList());
+  });
+
+  return (
+    <div className="private-chat-wrapper">
+      <div className="private-chat-wrapper-content">
+        <div className="private-chat-wrapper-content-side">
+          <ChatList />
+        </div>
+        <div className="private-chat-wrapper-content-conversation">
+          {(selectedChatUser || chatList.length > 0) && 'Chat window'}
+          {!selectedChatUser && !chatList.length && (
+            <div className="no-chat">Select or Search for users to chat with</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Chat;
